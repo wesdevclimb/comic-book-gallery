81ed4449ed110cdc3e288e813730378f47217288
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-01 18:17:10 -0500

    Added ComicBooks Detail View

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if(DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the ComicBooksController!");
+            return View();
         }
     }
 }
