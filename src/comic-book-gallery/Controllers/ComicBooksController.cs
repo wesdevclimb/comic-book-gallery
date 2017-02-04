1deb249863605a8804fdf109be3d8381000ccb37
wesdevclimb <wesleyjohnson.stu@gmail.com>
2017-02-04 11:31:54 -0600

    Did some sheet

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -9,22 +9,7 @@ namespace ComicBookGallery.Controllers
     public class ComicBooksController : Controller
     {
         public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Inks", Role = "Victor Olazaba" },
-                    new Artist() { Name = "Colors", Role = "Edgar Delgado" },
-                    new Artist() { Name = "Letters", Role = "Chris Eliopoulos" }
-                }
-            };    
-
+        {           
             return View(comicBook);
         }
     }
---
+++ src/comic-book-gallery/Data/ComicBookRepository.cs
@@ -0,0 +1,31 @@
+﻿using ComicBookGallery.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        public ComicBook GetComicBook()
+        {
+            var comicBook = new ComicBook()
+            {
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+               {
+                    new Artist() { Name = "Dan Slott", Role = "Script" },
+                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
+                    new Artist() { Name = "Inks", Role = "Victor Olazaba" },
+                    new Artist() { Name = "Colors", Role = "Edgar Delgado" },
+                    new Artist() { Name = "Letters", Role = "Chris Eliopoulos" }
+               }
+            };
+
+            return comicBook;
+        }
+    }
+}
